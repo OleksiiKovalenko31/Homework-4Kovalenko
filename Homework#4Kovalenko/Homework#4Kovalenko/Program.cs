e97eb9e3ee7230b629bf3ac5687ba8f4c4acac87
Oleksii Kovalenko <oleksii.kovalenko31@gmail.com>
2024-11-18 11:34:27 +0100

    Add  coments/Додавання коментарів

--- Homework#4Kovalenko/Homework#4Kovalenko/Program.cs
+++ Homework#4Kovalenko/Homework#4Kovalenko/Program.cs
@@ -1,63 +1,62 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.ComponentModel;
 using static System.Runtime.InteropServices.JavaScript.JSType;
-
+// Завдання перше
 Console.WriteLine("В этом приложении есть возможность вводить некорретные данные. Попробуйте.");
 Console.ReadKey();
-
+// Створюємо змінні
 string inOperandFirst;
 string inOperandSecond;
 int isNumber1;
 int isNumber2;
 
 Console.WriteLine("Здраствуйте это примитивный калькулятор.\n\nВведите выражение !\n\nПожайста для каждого ввода нажимайте ENTER!");
-Found:
+Found: //мітка з якої точки повторити введення даних
 
+
+// Введення даних та  Перевірка чи введенні дані є числом  перше число
 inOperandFirst = Console.ReadLine();
-bool success1 = int.TryParse(inOperandFirst, out isNumber1);
+bool success1 = int.TryParse(inOperandFirst, out isNumber1); 
 
+// Введння оператора разрахунку
 string inOperator = Console.ReadLine();
 Console.Clear();
 Console.WriteLine("{0} {1}", inOperandFirst, inOperator);
 
+// Введення даних та  Перевірка чи введенні дані є числом  друге
 inOperandSecond = Console.ReadLine();
 Console.Clear();
 Console.WriteLine("{0} {1} {2}", inOperandFirst, inOperator, inOperandSecond);
-
-bool success2 = int.TryParse(inOperandSecond, out isNumber2);
-
+bool success2 = int.TryParse(inOperandSecond, out isNumber2);  
 
 
-switch (success1 && success2)
+// Обчислення
+switch (success1 && success2) 
 {
-case true:
-switch (inOperator)
-{
-case "+":
-Console.WriteLine("Равно  {0}", isNumber1 + isNumber2);
-break;
-case "-":
-Console.WriteLine("Равно  {0}", isNumber1 - isNumber2);
-break;
-case "*":
-Console.WriteLine("Равно  {0}", isNumber1 * isNumber2);
-break;
-case "/":
-Console.WriteLine("Равно  {0}", Convert.ToDecimal(isNumber1) / Convert.ToDecimal(isNumber2));
-break;
+    case true:
+        switch (inOperator)
+        {
+            case "+": Console.WriteLine("Равно  {0}", isNumber1 + isNumber2);
+                break;
+            case "-": Console.WriteLine("Равно  {0}", isNumber1 - isNumber2);
+                 break;
+            case "*": Console.WriteLine("Равно  {0}", isNumber1 * isNumber2);
+                break;
+            case "/": Console.WriteLine("Равно  {0}", Convert.ToDecimal(isNumber1) / Convert.ToDecimal(isNumber2));
+                break;
 
+        }
+         break;
+
+
+    case false: //якщо данні не є числом повертаємось на мітку  старту
+
+        Console.Clear();
+        Console.WriteLine("Это символы или недопустимый набор знаков введите цифры и оператор!\n\nПопробуйте еще раз . . .\n");
+        goto Found;
 }
-break;
-
-
-case false:
-
-Console.Clear();
-Console.WriteLine("Это символы или недопустимый набор знаков введите цифры и оператор!\n\nПопробуйте еще раз . . .\n");
-goto Found;
-}
-
-Found1:
+// Завдання друге
+Found1: // мітка порверння
 Console.WriteLine("Для продолжения нажмите ENTER!");
 
 Console.ReadKey();
@@ -65,14 +64,16 @@ Console.Clear();
 Console.WriteLine("Это счетик дней недели!\n\nПожалуйста введите номер от 1 до 7 !");
 
 string isNumberDay = Console.ReadLine();
+// Перевірка чи введені дані є числом
 bool isDay = int.TryParse(isNumberDay, out int isDayWeek);
+// Перевірка чи введенечисло є в межах 7 днів
 if (isDayWeek > 7)
 {
     Console.WriteLine("В неделе только 7 дней. Пожалуйста введите номер от 1 до 7 ! ");
 
-    goto Found1;
+    goto Found1; // якщо більше семи повертаємось на момент ввдення
 }
-
+// Виводемо назву дня в залежності від номеру
 switch (isDay)
 {
     case true:switch(isDayWeek)
@@ -93,6 +94,7 @@ switch (isDay)
                 break;
         };
         break;
+        // якщо введені дані не є чмслом вертаємось до міткм введення даних
     case false: Console.WriteLine("Это не номер а символы.\n\nПожалуйста введите номер от 1 до 7 !");
         goto Found1;
         //break;
